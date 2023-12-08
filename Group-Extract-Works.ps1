df8c6f171d440541dc8d2bea64666865f7442838
tromoe <146355434+tromoe@users.noreply.github.com>
2023-12-08 14:15:50 +0100

    Group extract skript
    Skript som drar ut alla grupper i resources, stockholm och sundsvall (for now)

---
+++ Group-Extract-Works.ps1
@@ -0,0 +1,4 @@
+﻿$Filepath=$args[0]
+Get-ADGroup -SearchBase "OU=ResourceGroups,DC=lunchrummet,DC=nu" -Filter * > $Filepath
+Get-ADGroup -SearchBase "OU=Stockholm,DC=lunchrummet,DC=nu" -Filter * >> $Filepath
+Get-ADGroup -SearchBase "OU=Sundsvall,DC=lunchrummet,DC=nu" -Filter * >> $Filepath
